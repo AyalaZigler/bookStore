c0b9ed383f4fcfbf46e9a84e79ebf974a7d6987d
AyalaZigler <ayala52253@gmail.com>
2025-07-29 20:19:43 +0300

    update the port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const booksData = require('./src/booksData');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // מסד זמני בזיכרון
 let books = booksData;
